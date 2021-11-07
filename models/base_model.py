72527c1d818ddf13024075bdf02bfe7031a61605
Juan Bocanegra <jbocane6@gmail.com>
2021-11-07 17:14:39 -0500

    base_model task 4 v.1

--- models/base_model.py
+++ models/base_model.py
@@ -8,10 +8,21 @@ from datetime import datetime
 class BaseModel():
     """Defines all common attributes/methods."""
 
-    def __init__(self):
-        """Defines all common attributes."""
-        self.id = str(uuid.uuid4())
-        self.created_at = self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """Defines all common attributes.
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments."""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        value = datetime.strptime(value,
+                                                  '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
+        else:            
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns info about class."""

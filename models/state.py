b73e313dec6c21680361b05cb503902d22323589
Juan Bocanegra <jbocane6@gmail.com>
2021-11-07 22:22:44 -0500

    task 9 v.1

---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Class State that inherits from BaseModel."""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Class State that inherits from BaseModel."""
+    name = ""
